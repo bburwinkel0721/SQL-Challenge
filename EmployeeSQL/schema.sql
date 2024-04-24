84caad0efd1a68739cf5a61db6e4a70bf7edee25
bburwinkel <burwinkelben1@gmail.com>
2024-04-24 07:39:11 -0400

    finished the rough draft of my schema

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,3 +1,4 @@
+-- drops the tables if they were already created
 DROP TABLE IF EXISTS departments;
 DROP TABLE IF EXISTS dept_emp;
 DROP TABLE IF EXISTS dept_manager;
@@ -5,32 +6,43 @@ DROP TABLE IF EXISTS employees;
 DROP TABLE IF EXISTS salaries;
 DROP TABLE IF EXISTS titles;
 
-
+-- this schema creates a table for the department csv
 CREATE TABLE departments (
-  dept_no VARCHAR(4) PRIMARY KEY NOT NULL,
-  dept_name VARCHAR(45) NOT NULL
+    dept_no VARCHAR(4) PRIMARY KEY NOT NULL,
+    dept_name VARCHAR(45) NOT NULL
 );
 
+-- this schema creates a table for the dept_emp csv
 CREATE TABLE dept_emp (
-  emp_no INT PRIMARY KEY NOT NULL,
-  dept_no VARCHAR(4) NOT NULL
+    emp_no INT PRIMARY KEY NOT NULL,
+    dept_no VARCHAR(4) NOT NULL
 );
 
+-- this schema creates a table for the dept_manager csv
 CREATE TABLE dept_manager (
-  emp_no INT PRIMARY KEY NOT NULL,
-  dept_no VARCHAR(4) NOT NULL
+    emp_no INT PRIMARY KEY NOT NULL,
+    dept_no VARCHAR(4) NOT NULL
 );
 
+-- this schema creates a table for the employees csv
 CREATE TABLE employees (
-  emp_no INT PRIMARY KEY NOT NULL,
-  emp_title VARCHAR(5) NOT NULL,
-
+    emp_no INT PRIMARY KEY NOT NULL,
+    emp_title_id VARCHAR(5) NOT NULL,
+    birth_date DATE,
+    first_name VARCHAR(20),
+    last_name VARCHAR(20),
+    sex VARCHAR(1),
+    hire_date DATE
 );
 
+-- this schema creates a table for the salaries csv
 CREATE TABLE salaries (
-
+    emp_no INT PRIMARY KEY NOT NULL,
+    salary INT NOT NULL
 );
 
+-- this schema creates a table for the titles csv
 CREATE TABLE titles (
-
+    title_id VARCHAR(5) PRIMARY KEY,
+    title VARCHAR(45)
 );
