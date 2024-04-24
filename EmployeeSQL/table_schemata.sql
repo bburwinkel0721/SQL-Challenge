3fd3fa556f59aefcc7146e7628ebbc3a58f4b83b
bburwinkel <burwinkelben1@gmail.com>
2024-04-24 18:26:56 -0400

    added composite and not nulls

--- EmployeeSQL/table_schemata.sql
+++ EmployeeSQL/table_schemata.sql
@@ -26,13 +26,14 @@ CREATE TABLE employees (
     first_name VARCHAR(20),
     last_name VARCHAR(20),
     sex VARCHAR(1),
-    hire_date DATE,
+    hire_date DATE NOT NULL,
     FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 );
 -- this schema creates a table for the dept_emp csv
 CREATE TABLE dept_emp (
-    emp_no INT,
-    dept_no VARCHAR(4),
+    emp_no INT NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
+    PRIMARY KEY (emp_no, dept_no),
     FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
@@ -48,6 +49,6 @@ CREATE TABLE dept_manager (
 -- this schema creates a table for the salaries csv
 CREATE TABLE salaries (
     emp_no INT PRIMARY KEY NOT NULL,
-    salary INT NOT NULL,
+    salary INT,
     FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
